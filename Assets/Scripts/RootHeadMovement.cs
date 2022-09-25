fe87e0e8ecf8b9c64b4c529eb2cf884d1cb52e40
Артемий <88839467+Ystvlxst@users.noreply.github.com>
2022-09-25 19:16:37 +0500

    2 variants printer prefabs

--- Assets/Scripts/RootHeadMovement.cs
+++ Assets/Scripts/RootHeadMovement.cs
@@ -1,13 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+using RunnerMovementSystem;
+using RunnerMovementSystem.Examples;
 using UnityEngine;
 
 public class RootHeadMovement : MonoBehaviour
 {
-    [SerializeField] private Transform _head;
+    [SerializeField] private MovementSystem _headMovementSystem;
+    [SerializeField] private MouseInput _mouseInput;
+    [SerializeField] RoadSegment _roadSegment1;
+    [SerializeField] RoadSegment _roadSegment2;
 
     private void Update()
     {
-        
+        if (_headMovementSystem.CurrentRoad == _roadSegment1 || _headMovementSystem.CurrentRoad == _roadSegment2)
+            _mouseInput.enabled = true;
+        else
+            _mouseInput.enabled = false;
     }
 }
