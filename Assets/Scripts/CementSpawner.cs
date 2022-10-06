dc5fb65658f21f085ea6af56179a05e99a56cc72
Артемий <88839467+Ystvlxst@users.noreply.github.com>
2022-10-06 16:09:13 +0500

    obi package, new scene

---
+++ Assets/Scripts/CementSpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Obi;
+
+public class CementSpawner : MonoBehaviour
+{
+    [SerializeField] private ObiEmitter _obiEmitter;
+
+    private void Awake()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/Vaporator.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using Obi;
+using static Obi.ObiSolver;
+
+public class Vaporator : MonoBehaviour
+{
+    [SerializeField] private ObiSolver _solver;
+    [SerializeField] private ParticleSystem _smoke;
+    //[SerializeField] private SmokeParticlePool _smokeParticlePool;
+
+
+    private void Start()
+    {
+        _solver.OnCollision += Solver_OnCollision;
+    }
+
+    private void OnEnable()
+    {
+        if(_solver != null)
+            _solver.OnCollision += Solver_OnCollision;
+    }
+
+    private void OnDisable()
+    {
+        _solver.OnCollision -= Solver_OnCollision;
+    }
+
+    public void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void Solver_OnCollision(object sender, ObiSolver.ObiCollisionEventArgs collisionEvent)
+    {
+        var world = ObiColliderWorld.GetInstance();
+
+        foreach (Oni.Contact contact in collisionEvent.contacts)
+        {
+            if (contact.distance < 0.01)
+            {
+                ObiColliderBase collider = world.colliderHandles[contact.bodyB].owner;
+
+                if (collider != null && collider.gameObject.TryGetComponent(out Vaporator _))
+                {
+                    int particleIndex = _solver.simplices[contact.bodyA];
+                    ParticleInActor particle = _solver.particleToActor[particleIndex];
+
+                    if (_smoke != null)
+                    { 
+                        //if(_smokeParticlePool.TryGetSmokeParticles(out ParticleSystem particleSystem))
+                        //{
+                        //   Vector3 pos = _solver.renderablePositions[particleIndex];
+                        //    pos.z = -5f;
+                        //  particleSystem.transform.position = pos;
+                        //}
+                    }
+
+                    particle.actor.DeactivateParticle(particle.indexInActor);
+                }
+            }
+        }
+    }
+}
